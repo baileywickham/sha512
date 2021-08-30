5a98e64fabe972f4cad4b792b15fe4f0b0afd9a3
baileywickham <baileywickham@gmail.com>
2021-08-29 20:41:19 -1000

    refactor

--- sha.go
+++ sha.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"math"
 )
 
 const w = 64
 
-var K = [80]uint64{0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2, 0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65, 0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5, 0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df, 0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b, 0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8, 0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec, 0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b, 0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b, 0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817}
-var H = [8]uint64{0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179}
+var K = [...]uint64{0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc, 0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118, 0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2, 0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694, 0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65, 0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5, 0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4, 0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70, 0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df, 0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b, 0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30, 0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8, 0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8, 0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3, 0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec, 0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b, 0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178, 0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b, 0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c, 0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817}
 var H_I = 1
 
 func main() {
-	message := pad(messageToBytes("abc"))
+	H := []uint64{0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179}
+	fmt.Println(i64tobarr(200))
+	message := pad(messageToBytes(""))
 	Message := byteSliceToMessage(message)
-	hash(Message)
-	printHash()
+	fmt.Println(Message)
+	fmt.Println(len(Message[0]))
+	H = hash(Message, H)
+	printHash(H)
 }
 
-func printHash() {
+func printHash(H []uint64) {
 	output := ""
 	for i := 0; i <= 7; i++ {
 		output += fmt.Sprintf("%x", H[i])
@@ -40,7 +44,7 @@ func shr(n, x uint64) uint64 {
 	return x >> n
 
 }
-func ch(x, y, z uint64) uint64 {
+func Ch(x, y, z uint64) uint64 {
 	return (x & y) ^ ((^x) & z)
 }
 
@@ -75,18 +79,12 @@ func add(nums ...uint64) uint64 {
 
 func i64tobarr(val uint64) []byte {
 	r := make([]byte, 8)
-	for i := uint64(0); i < 8; i++ {
-		r[i] = byte((val >> (i * 8)) & 0xff)
-	}
+	binary.LittleEndian.PutUint64(r, val)
 	return r
 }
 
 func btoi64(val []byte) uint64 {
-	r := uint64(0)
-	for i := uint64(0); i < 8; i++ {
-		r |= uint64(val[i]) << (8 * i)
-	}
-	return r
+	return binary.LittleEndian.Uint64(val)
 }
 
 // returns a padded message which should be a multiple of 1024
@@ -94,6 +92,7 @@ func pad(message []byte) []byte {
 	messageLenBytes := uint64(len(message) * 8)
 	k := (896 - (messageLenBytes + 1)) % 1024
 
+	// Append a 1, then k -1 bits of 0s.
 	zero_pad := ((k - 1) / 8)
 	// append 1 lengthened to a byte
 	message = append(message, (byte(0)+1)<<7)
@@ -121,13 +120,14 @@ func byteSliceToMessage(message []byte) [][]uint64 {
 	return Message
 }
 
-func hash(Message [][]uint64) {
+func hash(Message [][]uint64, H []uint64) []uint64 {
 	for _, value := range Message {
-		hashMessage(value)
+		H = hashMessage(value, H)
 	}
+	return H
 }
 
-func hashMessage(messageBlock []uint64) {
+func hashMessage(messageBlock []uint64, H []uint64) []uint64 {
 	W := make([]uint64, 80)
 	for t := 0; t <= 15; t++ {
 		W[t] = messageBlock[t]
@@ -146,7 +146,7 @@ func hashMessage(messageBlock []uint64) {
 	g := H[6]
 	h := H[7]
 	for t := 0; t <= 79; t++ {
-		T_1 := add(h, SIG_1(e), ch(e, f, g), K[t], W[t])
+		T_1 := add(h, SIG_1(e), Ch(e, f, g), K[t], W[t])
 		T_2 := add(SIG_0(a), Maj(a, b, c))
 		h = g
 		g = f
@@ -155,7 +155,7 @@ func hashMessage(messageBlock []uint64) {
 		d = c
 		c = b
 		b = a
-		a = add(T_1 + T_2)
+		a = add(T_1, T_2)
 	}
 	H[0] = add(a, H[0])
 	H[1] = add(b, H[1])
@@ -165,6 +165,7 @@ func hashMessage(messageBlock []uint64) {
 	H[5] = add(f, H[5])
 	H[6] = add(g, H[6])
 	H[7] = add(h, H[7])
+	return H
 }
 
 func messageToBytes(message string) []byte {
